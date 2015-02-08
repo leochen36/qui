98cceb8f066c0efeaf614264b5621ed5e8b4cd27
leo <cwq0312@163.com>
2015-02-08 08:40:15 +0800

    add

--- src/qui.js
+++ src/qui.js
@@ -7,8 +7,8 @@
         '<div class="qui-p-wrap  ${dialog}">' +
         '<a href="javascript:" class="qui-p-close qui-icon">x</a>' +
         '<div class="qui-p-head">${title} </div>' +
-        '<div class="qui-p-body"><i class="qui-icon qui-icon-${dialogType} qui-icon-sprite"></i><div class="qui-msg">${msg}</div> </div>' +
-        '<div class="qui-p-foot"><c class="qui-btn qui-btn-${dialogType}">确定</c> </div>' +
+        '<div class="qui-p-body"><i class="qui-icon qui-icon-${dialogType} qui-icon-sprite"></i><div class="qui-msg-content">${msg}</div> </div>' +
+        '<div class="qui-p-foot"><c class="qui-btn qui-btn-cancel" style="display:none;">取消</c><c class="qui-btn qui-btn-ok">确定</c></div>' +
         '</div>' +
         '</div>';
 
@@ -16,9 +16,12 @@
         shadeClose: true
     };
     var g_dialog_type = {
-        ok: "ok"
+        ok: "ok",
+        info: "info",
+        confirm: "confirm"
     };
     var g_css_keys = "width height";
+    var g_dialog;
 
     function UI() {
         Q(function () {
@@ -30,8 +33,23 @@
             });
             Q(".qui .qui-btn").live({
                 click: function (e) {
+                    var qme = Q(this);
+                    var qui = Q(this).closest(".qui");
+                    if (qui.hasClass("qui-alert") ||
+                        qui.hasClass("qui-confirm")) {
+                        var bool = qme.hasClass("qui-btn-ok");
+                        Q.execCatch(function () {
+                            qui[0].__quidialog && qui[0].__quidialog(bool);
+                        });
+                    }
                     Q(this).closest(".qui").remove();
                     Q(".qui-bg").remove();
+                }
+            });
+            Q(".qui-bg").live({
+                click: function (e) {
+                    Q(this).remove();
+                    g_dialog && g_dialog.remove();
                 }
             });
         });
@@ -40,25 +58,38 @@
     Q.extend(UI.prototype, {
         DIALOG_TYPE: g_dialog_type,
         msg: function (msg) {
-
+            var me = this;
+            Q(".qui-msg").remove();
+            g_dialog = makeDialog({
+                msg: msg,
+                type: 'qui-msg',
+                dialogType: me.DIALOG_TYPE.info
+            });
+            return g_dialog;
         },
         alert: function (msg, callback) {
             var me = this;
             Q(".qui-alert").remove();
             addBG();
-            return makeDialog({
+            g_dialog = makeDialog({
                 msg: msg,
                 type: 'qui-alert',
-                dialogType: me.DIALOG_TYPE.ok
+                dialogType: me.DIALOG_TYPE.info
             });
+            return g_dialog;
         },
         confirm: function (msg, callback) {
-            Q(".qui-alert").remove();
+            var me = this;
+            Q(".qui-confirm").remove();
             addBG();
-            make({
+            g_dialog = makeDialog({
                 msg: msg,
-                type: 'qui-confirm'
+                type: 'qui-confirm',
+                dialogType: me.DIALOG_TYPE.confirm,
+                callback: callback
             });
+            g_dialog.find(".qui-btn-cancel").css("display", "inline-block");
+            return g_dialog;
         },
         tip: function (dom, msg) {
 
@@ -79,8 +110,8 @@
     });
 
     function addBG() {
-        Q(".q-ui-bg").remove();
-        Q('body').append('<div class="q-ui-bg"></div>');
+        Q(".qui-bg").remove();
+        Q('body').append('<div class="qui-bg"></div>');
     }
 
     function replaceVar(template, data) {
@@ -97,12 +128,17 @@
             style[key] = param.style[key];
         });
         param.type = param.type + " qui-p";
-        param.dialog="qui-dialog";
+        param.dialog = "qui-dialog";
         var html = replaceVar(template, param);
         var qdom = Q(html);
         style = Q.cssPrefix(style);
         qdom.css(style);
         Q('body').append(qdom);
+        qdom.css({
+            left: (window.innerWidth - qdom.width()) / 2 + "px",
+            top: (window.innerHeight - qdom.height()) / 2 + "px"
+        });
+        qdom[0].__quidialog = param.callback;
         return qdom;
     }
 
--- view/demoTip.js
+++ view/demoTip.js
@@ -5,9 +5,12 @@ define("demoTip", function(require){
     var Module = {
         init: function(scope){
             scope.clickTip = function(e){
+
                 var router = $(e.target).attr("router");
-                $.ui.alert("卤素卤素卤素卤素卤素卤素卤素卤素<br/>卤素卤素卤素卤素卤素卤<br/>卤素卤素卤素卤素卤素卤素卤素");
-                e.stopPropagation();
+                var paths = router.split("/");
+                if(e.type == paths[0]){
+                    $.ui[paths[1]]("卤素卤素卤素卤素卤素卤素卤素卤素<br/>卤素卤素卤素卤素卤素卤<br/>卤素卤素卤素卤素卤素卤素卤素");
+                }
             }
         }
     };
